09eb720a6b8df04d9259cdbb7a4ed879a3dc957e
lorgauss <leeloo.junebug@gmail.com>
2022-10-11 12:55:03 +0200

    first commit

---
+++ front/js/confirmation.js
@@ -0,0 +1,15 @@
+(function Commande() {
+    
+    //Récupération de l'orderId localisé dans l'url
+    let orderId = new URLSearchParams(document.location.search).get("commande");
+    
+    //Affichage
+    document.querySelector("#orderId").innerHTML = `<br>${orderId}`;
+    //Debug log
+    console.log("orderId: " + orderId);
+
+    //Nettoyage des données stockées et réinisialisation du orderId
+    sessionStorage.clear();
+    localStorage.clear();
+    orderId = undefined;
+})();
